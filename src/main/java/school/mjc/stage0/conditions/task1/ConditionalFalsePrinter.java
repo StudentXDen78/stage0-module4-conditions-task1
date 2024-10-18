70dfd0db76ab2f29127bd65f4eafcf4dcd026ac7
xden78 <venera.opt@gmail.com>
2024-10-18 10:47:30 +0300

    attempt 2

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -3,12 +3,13 @@ package school.mjc.stage0.conditions.task1;
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
         int a = 1;
-        int b = 1;
+        int b = 2;
         boolean c;
         if (a == b) {
             c = true;
             System.out.println(c);
-        }         else {
+        }
+        else {
             c = false;
             System.out.println(c);
         }
